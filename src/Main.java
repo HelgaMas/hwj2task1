3ea122999d8f41ee84de1ea027f8d1d327b34c19
olgamasl <masloolg@gmail.com>
2022-02-02 11:53:31 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Всего бонусных миль:");
+        int ticketPrice = 5000;
+        int oneMile = 20;
+        int bonusMile = ticketPrice / oneMile;
+        System.out.println(bonusMile);
+
+
+    }
+}
